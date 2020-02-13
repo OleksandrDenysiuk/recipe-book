cb1b99ced10628fbbfc1ab9b40091c48e2346601
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-13 21:07:57 +0100

    Added Step entity

---
+++ src/main/java/com/portfolio/recipebook/model/Step.java
@@ -0,0 +1,24 @@
+package com.portfolio.recipebook.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "steps")
+public class Step extends BaseEntity{
+
+    @Lob
+    private Byte[] image;
+
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "manual_id", nullable = false)
+    private Manual manual;
+}
