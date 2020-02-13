8f4676d2a1b0bd29ec0f240695cc5d25bf522c85
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-13 22:11:55 +0100

    Added index page and controller for it

---
+++ src/main/java/com/portfolio/recipebook/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.portfolio.recipebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"","/","index.html"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
