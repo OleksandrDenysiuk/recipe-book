6868661ca9ed98cf4f281e7206e1c732a0aedcc7
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-15 15:36:55 +0100

    Added repository

---
+++ src/main/java/com/portfolio/recipebook/repository/ManualRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.recipebook.repository;
+
+import com.portfolio.recipebook.model.Manual;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ManualRepository  extends CrudRepository<Manual, Long> {
+}
