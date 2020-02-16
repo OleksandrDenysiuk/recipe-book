615c158020f33b1088a09e7545f648babbf6ac96
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-16 15:55:46 +0100

    Added saving method for Step.

--- src/main/java/com/portfolio/recipebook/service/ImageService.java
+++ src/main/java/com/portfolio/recipebook/service/ImageService.java
@@ -1,8 +1,11 @@
 package com.portfolio.recipebook.service;
 
 import com.portfolio.recipebook.model.Recipe;
+import com.portfolio.recipebook.model.Step;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
     void saveImageFile(Recipe recipe, MultipartFile file);
+
+    void saveImageFile(Step step, MultipartFile file);
 }
