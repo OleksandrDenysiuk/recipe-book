fd14de283b08c1d04d6c12863a1e4fe1e2e60e83
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-16 15:46:25 +0100

    Added upload image to Step

--- src/main/java/com/portfolio/recipebook/controller/ManualController.java
+++ src/main/java/com/portfolio/recipebook/controller/ManualController.java
@@ -3,15 +3,14 @@ package com.portfolio.recipebook.controller;
 import com.portfolio.recipebook.model.Manual;
 import com.portfolio.recipebook.model.Recipe;
 import com.portfolio.recipebook.model.Step;
+import com.portfolio.recipebook.service.ImageService;
 import com.portfolio.recipebook.service.ManualService;
 import com.portfolio.recipebook.service.StepService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 
@@ -20,10 +19,12 @@ public class ManualController {
 
     private final ManualService manualService;
     private final StepService stepService;
+    private final ImageService imageService;
 
-    public ManualController(ManualService manualService, StepService stepService) {
+    public ManualController(ManualService manualService, StepService stepService, ImageService imageService) {
         this.manualService = manualService;
         this.stepService = stepService;
+        this.imageService = imageService;
     }
 
     @GetMapping("recipe/{id}/manual")
@@ -40,8 +41,10 @@ public class ManualController {
     @PostMapping("recipe/{recipe}/manual/step/new")
     public String createStep(@PathVariable Recipe recipe,
                              @Valid @ModelAttribute Step step,
+                             @RequestParam("imageStep") MultipartFile image,
                              BindingResult result){
         stepService.saveAndSetToManual(step,recipe.getManual());
+        imageService.saveImageFile(step, image);
         return "redirect:/recipe/" + recipe.getId() + "/manual";
     }
 }
