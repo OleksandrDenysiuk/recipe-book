15c1c982942f2708e4fd515a2ca6f34ce7f41e05
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-16 15:55:22 +0100

    Added saving image method.

--- src/main/java/com/portfolio/recipebook/service/impl/ImageServiceImpl.java
+++ src/main/java/com/portfolio/recipebook/service/impl/ImageServiceImpl.java
@@ -1,7 +1,9 @@
 package com.portfolio.recipebook.service.impl;
 
 import com.portfolio.recipebook.model.Recipe;
+import com.portfolio.recipebook.model.Step;
 import com.portfolio.recipebook.repository.RecipeRepository;
+import com.portfolio.recipebook.repository.StepRepository;
 import com.portfolio.recipebook.service.ImageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,9 +18,11 @@ public class ImageServiceImpl implements ImageService {
 
 
     private final RecipeRepository recipeRepository;
+    private final StepRepository stepRepository;
 
-    public ImageServiceImpl( RecipeRepository recipeService) {
+    public ImageServiceImpl(RecipeRepository recipeService, StepRepository stepRepository) {
         this.recipeRepository = recipeService;
+        this.stepRepository = stepRepository;
     }
 
     @Override
@@ -37,6 +41,28 @@ public class ImageServiceImpl implements ImageService {
             recipe.setImage(byteObjects);
 
             recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.error("Error occurred", e);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    @Transactional
+    public void saveImageFile(Step step, MultipartFile file) {
+
+        try {
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            step.setImage(byteObjects);
+
+            stepRepository.save(step);
         } catch (IOException e) {
             log.error("Error occurred", e);
             e.printStackTrace();
