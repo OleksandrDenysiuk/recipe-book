d94ec0d4adfd48b777c15e94444a95adacdf60a6
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-27 17:20:01 +0300

    Add Ingredient repository

---
+++ src/main/java/com/portfolio/recipebook/repository/IngredientRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.recipebook.repository;
+
+import com.portfolio.recipebook.model.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
+}
