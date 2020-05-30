cafeef9891d63c33e4f857610f81051c93290832
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-30 13:30:15 +0300

    Add annotations to validation

--- src/main/java/com/portfolio/recipebook/command/StepCommand.java
+++ src/main/java/com/portfolio/recipebook/command/StepCommand.java
@@ -1,5 +1,6 @@
 package com.portfolio.recipebook.command;
 
+import com.portfolio.recipebook.validator.Image;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Setter;
 import org.hibernate.annotations.Type;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -17,8 +19,10 @@ public class StepCommand {
 
     private Long id;
 
+    @Image
     private MultipartFile image;
 
+    @NotBlank
     @Type(type = "text")
     @Size(max = 350)
     private String description;
