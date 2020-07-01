b97adec2f142c99b1ca6272316253361f99af2d3
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-07-01 14:50:36 +0200

    Rewrite tests

--- src/test/java/com/portfolio/recipebook/service/impl/IngredientServiceImplTest.java
+++ src/test/java/com/portfolio/recipebook/service/impl/IngredientServiceImplTest.java
@@ -1,19 +1,21 @@
 package com.portfolio.recipebook.service.impl;
 
+import com.portfolio.recipebook.command.IngredientCommand;
+import com.portfolio.recipebook.dto.IngredientDto;
 import com.portfolio.recipebook.model.Ingredient;
 import com.portfolio.recipebook.model.Recipe;
+import com.portfolio.recipebook.repository.IngredientRepository;
 import com.portfolio.recipebook.repository.RecipeRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
@@ -22,60 +24,64 @@ class IngredientServiceImplTest {
 
     @Mock
     RecipeRepository recipeRepository;
+    @Mock
+    IngredientRepository ingredientRepository;
 
     @InjectMocks
     IngredientServiceImpl ingredientService;
 
-
-    @BeforeEach
-    void setUp() {
-    }
-
     @Test
-    void findByIngredientIdAndRecipeId() {
+    void getAllByRecipeId(){
         Recipe recipe = Recipe.builder().id(1L).build();
-
-        Long recipeId = recipe.getId();
-        Long ingredientId = 1L;
-
-        recipe.addIngredient(Ingredient.builder().id(1L).build());
+        Ingredient ingredient1 = Ingredient.builder().id(1L).build();
+        ingredient1.setRecipe(recipe);
+        Ingredient ingredient2 = Ingredient.builder().id(2L).build();
+        ingredient2.setRecipe(recipe);
+        recipe.getIngredients().add(ingredient1);
+        recipe.getIngredients().add(ingredient2);
 
         when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
 
-        Ingredient ingredient = ingredientService
-                .findOne(ingredientId, recipeId);
+        List<IngredientDto> ingredientDtoList = ingredientService.getAllByRecipeId(1L);
 
-        assertNotNull(ingredient);
+        assertEquals(2, ingredientDtoList.size());
 
         verify(recipeRepository,times(1)).findById(anyLong());
-        verify(recipeRepository,never()).findAll();
-
-        assertEquals(ingredient.getId(),ingredientId);
     }
 
     @Test
-    void save() {
+    void create() {
         Recipe recipe = Recipe.builder().id(1L).build();
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setName("name");
+        ingredientCommand.setAmount(2);
+        ingredientCommand.setMeasure("TEASPOON");
+        Ingredient ingredient = Ingredient.builder().id(1L).build();
+        ingredient.setRecipe(recipe);
 
         when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
+        when(ingredientRepository.save(any())).thenReturn(ingredient);
 
-        ingredientService.save(Ingredient.builder().id(1L).build(), 1L);
+        IngredientDto ingredientDto = ingredientService.create(ingredientCommand, 1L);
 
-        verify(recipeRepository, times(1)).findById(anyLong());
-        verify(recipeRepository, times(1)).save(any(Recipe.class));
-
+        assertNotNull(ingredientDto);
         assertEquals(1,recipe.getIngredients().size());
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(ingredientRepository, times(1)).save(any());
     }
 
     @Test
-    void deleteById() {
+    void delete() {
         Recipe recipe = Recipe.builder().id(1L).build();
-        recipe.addIngredient(Ingredient.builder().id(1L).build());
+        Ingredient ingredient = Ingredient.builder().id(1L).build();
+        ingredient.setRecipe(recipe);
+        recipe.addIngredient(ingredient);
 
         when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
 
-        ingredientService.deleteOne(1L,1L);
+        ingredientService.delete(1L,1L);
 
+        assertNull(ingredient.getRecipe());
         assertEquals(0,recipe.getIngredients().size());
     }
 }
--- src/test/java/com/portfolio/recipebook/service/impl/StepServiceImplTest.java
+++ src/test/java/com/portfolio/recipebook/service/impl/StepServiceImplTest.java
@@ -1,18 +1,25 @@
 package com.portfolio.recipebook.service.impl;
 
+import com.portfolio.recipebook.command.StepCommand;
+import com.portfolio.recipebook.dto.StepDto;
 import com.portfolio.recipebook.model.Recipe;
 import com.portfolio.recipebook.model.Step;
 import com.portfolio.recipebook.repository.RecipeRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.portfolio.recipebook.repository.StepRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
@@ -21,60 +28,160 @@ class StepServiceImplTest {
 
     @Mock
     RecipeRepository recipeRepository;
+    @Mock
+    StepRepository stepRepository;
 
     @InjectMocks
     StepServiceImpl stepService;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void getAll(){
+        Long recipeId = 1L;
+        Recipe recipe = Recipe.builder().id(recipeId).build();
+        Step step1 = Step.builder().id(1L).build();
+        step1.setRecipe(recipe);
+        Step step2 = Step.builder().id(2L).build();
+        step1.setRecipe(recipe);
+        recipe.getSteps().add(step1);
+        recipe.getSteps().add(step2);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
+
+        List<StepDto> stepDtoList = stepService.getAllByRecipeId(recipeId);
+
+        assertEquals(2, stepDtoList.size());
+        verify(recipeRepository, times(1)).findById(anyLong());
     }
 
     @Test
-    void findOne() {
+    void getOneByIdAndRecipeId() {
         Long stepId = 1L;
-        Recipe recipe = Recipe.builder().id(1L).build();
-        recipe.getSteps().add(Step.builder().id(stepId).build());
+        Long recipeId = 1L;
+        Recipe recipe = Recipe.builder().id(recipeId).build();
+        Step step = Step.builder().id(stepId).build();
+        step.setRecipe(recipe);
+        recipe.getSteps().add(step);
 
-        Optional<Recipe> recipeOptional = Optional.of(recipe);
+        when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
 
-        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        StepDto stepDto = stepService.getOneByIdAndRecipeId(stepId, recipe.getId());
 
-        Step step = stepService.findOne(stepId, recipe.getId());
-
+        assertNotNull(stepDto);
         assertEquals(stepId, step.getId());
         verify(recipeRepository,times(1)).findById(anyLong());
     }
 
+
     @Test
-    void deleteOne() {
-        Long stepId = 1L;
+    void create() {
         Recipe recipe = Recipe.builder().id(1L).build();
-        recipe.getSteps().add(Step.builder().id(stepId).build());
 
-        Optional<Recipe> recipeOptional = Optional.of(recipe);
+        StepCommand stepCommand = new StepCommand();
+        stepCommand.setDescription("description");
+        stepCommand.setImage(getFile());
+        Step step = Step.builder().id(1L).build();
+        step.setRecipe(recipe);
 
-        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
+        when(stepRepository.save(any())).thenReturn(step);
 
-        stepService.deleteOne(stepId, recipe.getId());
+        StepDto stepDto = stepService.create(stepCommand, recipe.getId());
 
+        assertNotNull(stepDto);
+        assertEquals(1L,stepDto.getId());
         verify(recipeRepository,times(1)).findById(anyLong());
-        verify(recipeRepository,times(1)).save(any(Recipe.class));
-
-        assertEquals(0,recipe.getSteps().size());
+        verify(stepRepository,times(1)).save(any());
     }
 
     @Test
-    void save() {
-        Step stepToSave = Step.builder().id(1L).build();
+    void update() {
+        Long stepId = 1L;
+        Long recipeId = 1L;
+        Recipe recipe = Recipe.builder().id(recipeId).build();
+        Step step = Step.builder()
+                .id(stepId)
+                .description("description")
+                .build();
+        step.setRecipe(recipe);
+        recipe.getSteps().add(step);
 
-        Recipe recipe = Recipe.builder().id(1L).build();
+        StepCommand stepCommand = new StepCommand();
+        stepCommand.setId(stepId);
+        stepCommand.setDescription("description2");
+        stepCommand.setImage(getFile());
 
-        Optional<Recipe> recipeOptional = Optional.of(recipe);
+        when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
+        when(stepRepository.save(any())).thenReturn(step);
 
-        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        StepDto stepDto = stepService.update(stepCommand, recipeId);
 
-        Step step = stepService.save(stepToSave,recipe.getId());
+        assertNotNull(stepDto);
+        assertEquals(1L, stepDto.getId());
+        assertEquals("description2",stepDto.getDescription());
+        verify(recipeRepository,times(1)).findById(anyLong());
+        verify(stepRepository,times(1)).save(any());
+    }
 
-        assertEquals(1L,step.getId());
+    @Test
+    void delete() {
+        Long stepId = 1L;
+        Long recipeId = 1L;
+        Recipe recipe = Recipe.builder().id(recipeId).build();
+        Step step = Step.builder().id(stepId).build();
+        step.setRecipe(recipe);
+        recipe.getSteps().add(step);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
+
+        stepService.delete(stepId, recipeId);
+
+        assertEquals(0,recipe.getSteps().size());
+        assertNull(step.getRecipe());
+        verify(recipeRepository,times(1)).findById(anyLong());
+        verify(stepRepository,times(1)).delete(any());
+    }
+
+    private MultipartFile getFile(){
+        return  new MultipartFile() {
+            @Override
+            public String getName() {
+                return "image";
+            }
+
+            @Override
+            public String getOriginalFilename() {
+                return null;
+            }
+
+            @Override
+            public String getContentType() {
+                return null;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public long getSize() {
+                return 0;
+            }
+
+            @Override
+            public byte[] getBytes() throws IOException {
+                return new byte[0];
+            }
+
+            @Override
+            public InputStream getInputStream() throws IOException {
+                return null;
+            }
+
+            @Override
+            public void transferTo(File file) throws IOException, IllegalStateException {
+
+            }
+        };
     }
 }
